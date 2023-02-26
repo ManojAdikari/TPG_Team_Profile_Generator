f597cf4428b0153073b2b1a14cd2d170313ad00f
Manoj Adikari <manojpadikari@gmail.com>
2023-02-26 14:13:05 +1030

    add index.js file

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -14,15 +14,15 @@ describe("get_Office_Number", () => {
     it("Displays Office Number", () => {
         const officeNumber = 123;
         const manager = new Manager("manoj", 1, "manoj@gmail.com", officeNumber);
-        expect(manager.get_Office_Number()).toBe(officeNumber);
+        expect(manager.getOfficeNumber()).toBe(officeNumber);
     });
 });
 
-describe("get_Role", () => {
+describe("getRole", () => {
     it("Displays Role", () => {
         const role = "Manager";
         const manager = new Manager("manoj", 1, "manoj@gmail.com", 123);
-        expect(manager.get_Role()).toBe(role);
+        expect(manager.getRole()).toBe(role);
     });
 });
 });
